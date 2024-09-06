9b89200693bf4feafefba84f72fec216ba8f0934
wrightrebecca <r3beccawright05@gmail.com>
2024-09-06 13:10:21 +0100

    inner HTML date and times for Barcelona and Chicago working

--- JS/script.js
+++ JS/script.js
@@ -5,3 +5,23 @@ tokyoElement.innerHTML = tokyoDate;
 let tokyoTime = moment().tz("Asia/Tokyo").format("h:mm[<small>]A [</small>]");
 let tokyoTimeElement = document.querySelector("#tokyoTime");
 tokyoTimeElement.innerHTML = tokyoTime;
+
+barcelonaDate = moment().tz("Europe/Madrid").format("dddd, Do MMMM yy");
+let barcelonaElement = document.querySelector("#barcelonaDate");
+barcelonaElement.innerHTML = barcelonaDate;
+
+let barcelonaTime = moment()
+  .tz("Europe/Madrid")
+  .format("h:mm[<small>]A [</small>]");
+let barcelonaTimeElement = document.querySelector("#barcelonaTime");
+barcelonaTimeElement.innerHTML = barcelonaTime;
+
+chicagoDate = moment().tz("America/Chicago").format("dddd, Do MMMM yy");
+let chicagoElement = document.querySelector("#chicagoDate");
+chicagoElement.innerHTML = chicagoDate;
+
+let chicagoTime = moment()
+  .tz("America/Chicago")
+  .format("h:mm[<small>]A [</small>]");
+let chicagoTimeElement = document.querySelector("#chicagoTime");
+chicagoTimeElement.innerHTML = chicagoTime;
