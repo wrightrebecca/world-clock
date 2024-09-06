3c6b01ffb39771bd437e7ec62ddad5523d01404f
wrightrebecca <r3beccawright05@gmail.com>
2024-09-06 12:59:25 +0100

    inner HTML for Tokyo date and time is working

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,7 @@
+tokyoDate = moment().tz("Asia/Tokyo").format("dddd, Do MMMM yy");
+let tokyoElement = document.querySelector("#tokyoDate");
+tokyoElement.innerHTML = tokyoDate;
 
+let tokyoTime = moment().tz("Asia/Tokyo").format("h:mm[<small>]A [</small>]");
+let tokyoTimeElement = document.querySelector("#tokyoTime");
+tokyoTimeElement.innerHTML = tokyoTime;
