42dbc51f860b210192ee0d1b82d7a8a0e0867536
wrightrebecca <r3beccawright05@gmail.com>
2024-09-06 13:19:14 +0100

    Set interval for times and added seconds

--- JS/script.js
+++ JS/script.js
@@ -1,27 +1,30 @@
-tokyoDate = moment().tz("Asia/Tokyo").format("dddd, Do MMMM yy");
-let tokyoElement = document.querySelector("#tokyoDate");
-tokyoElement.innerHTML = tokyoDate;
+function updateTime() {
+  tokyoDate = moment().tz("Asia/Tokyo").format("dddd, Do MMMM yy");
+  let tokyoElement = document.querySelector("#tokyoDate");
+  tokyoElement.innerHTML = tokyoDate;
+  let tokyoTime = moment()
+    .tz("Asia/Tokyo")
+    .format("h:mm:ss[<small>] A [</small>]");
+  let tokyoTimeElement = document.querySelector("#tokyoTime");
+  tokyoTimeElement.innerHTML = tokyoTime;
 
-let tokyoTime = moment().tz("Asia/Tokyo").format("h:mm[<small>]A [</small>]");
-let tokyoTimeElement = document.querySelector("#tokyoTime");
-tokyoTimeElement.innerHTML = tokyoTime;
+  barcelonaDate = moment().tz("Europe/Madrid").format("dddd, Do MMMM yy");
+  let barcelonaElement = document.querySelector("#barcelonaDate");
+  barcelonaElement.innerHTML = barcelonaDate;
+  let barcelonaTime = moment()
+    .tz("Europe/Madrid")
+    .format("h:mm:ss[<small>] A [</small>]");
+  let barcelonaTimeElement = document.querySelector("#barcelonaTime");
+  barcelonaTimeElement.innerHTML = barcelonaTime;
 
-barcelonaDate = moment().tz("Europe/Madrid").format("dddd, Do MMMM yy");
-let barcelonaElement = document.querySelector("#barcelonaDate");
-barcelonaElement.innerHTML = barcelonaDate;
-
-let barcelonaTime = moment()
-  .tz("Europe/Madrid")
-  .format("h:mm[<small>]A [</small>]");
-let barcelonaTimeElement = document.querySelector("#barcelonaTime");
-barcelonaTimeElement.innerHTML = barcelonaTime;
-
-chicagoDate = moment().tz("America/Chicago").format("dddd, Do MMMM yy");
-let chicagoElement = document.querySelector("#chicagoDate");
-chicagoElement.innerHTML = chicagoDate;
-
-let chicagoTime = moment()
-  .tz("America/Chicago")
-  .format("h:mm[<small>]A [</small>]");
-let chicagoTimeElement = document.querySelector("#chicagoTime");
-chicagoTimeElement.innerHTML = chicagoTime;
+  chicagoDate = moment().tz("America/Chicago").format("dddd, Do MMMM yy");
+  let chicagoElement = document.querySelector("#chicagoDate");
+  chicagoElement.innerHTML = chicagoDate;
+  let chicagoTime = moment()
+    .tz("America/Chicago")
+    .format("h:mm:ss[<small>] A [</small>]");
+  let chicagoTimeElement = document.querySelector("#chicagoTime");
+  chicagoTimeElement.innerHTML = chicagoTime;
+}
+updateTime();
+setInterval(updateTime, 1000);
