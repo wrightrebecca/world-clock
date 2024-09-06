2cb39f2adc6768b704a54f2a813534401952cf35
wrightrebecca <r3beccawright05@gmail.com>
2024-09-06 15:21:34 +0100

    Used moment guess to display user's local timezone data

--- JS/script.js
+++ JS/script.js
@@ -25,6 +25,18 @@ function updateTime() {
     .format("h:mm:ss[<small>] A [</small>]");
   let chicagoTimeElement = document.querySelector("#chicagoTime");
   chicagoTimeElement.innerHTML = chicagoTime;
+
+  let currentDate = moment().format("dddd, Do MMMM yy");
+  let currentDateElement = document.querySelector("#currentLocationDate");
+  currentDateElement.innerHTML = currentDate;
+
+  let currentTime = moment().format("h:mm:ss[<small>] A [</small>]");
+  let currentTimeElement = document.querySelector("#currentLocationTime");
+  currentTimeElement.innerHTML = currentTime;
 }
 updateTime();
 setInterval(updateTime, 1000);
+
+let currentLocation = moment.tz.guess();
+let currentLocationElement = document.querySelector("#currentLocationName");
+currentLocationElement.innerHTML = currentLocation;
